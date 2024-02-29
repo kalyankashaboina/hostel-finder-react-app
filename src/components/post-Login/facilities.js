e5189d19e1fab9d7dd79e3e5dee3e0d10462f66d
kalyankashaboina <kalyankashaboina07@gmail.com>
2024-02-29 18:52:05 +0530

    updates

--- src/components/post-Login/facilities.js
+++ src/components/post-Login/facilities.js
@@ -1,6 +1,6 @@
 
 import React, {useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate} from 'react-router-dom'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
